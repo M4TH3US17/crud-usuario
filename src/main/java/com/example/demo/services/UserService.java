86cfef63826a0b21b3e65dcf053a4559999a9ffa
M4TH3US17 <matheusdalvino50@gmail.com>
2022-10-21 14:26:29 -0400

    UserService

--- src/main/java/com/example/demo/services/UserService.java
+++ src/main/java/com/example/demo/services/UserService.java
@@ -3,7 +3,8 @@ package com.example.demo.services;
 import com.example.demo.entities.User;
 import com.example.demo.entities.dto.UserDTO;
 import com.example.demo.repositories.UserRepository;
-import lombok.RequiredArgsConstructor;
+import lombok.*;
+import org.springframework.data.domain.*;
 import org.springframework.stereotype.Service;
 
 @Service @RequiredArgsConstructor
@@ -11,8 +12,30 @@ public class UserService {
 
     private final UserRepository repository;
 
-    public User save(UserDTO userDTO) {
-        return new User();
+    public Page<User> findAllUsers(Pageable pageable) {
+        return repository.findAll(pageable);
     }
 
+    public User findUserById(Long id) {
+        return repository.findById(id).get();
+    }
+
+    public User save(UserDTO userDTO) {
+        return repository.save(new User(null, userDTO.getLogin(), userDTO.getPassword()));
+    }
+
+    public void delete(Long id) {
+        repository.deleteById(id);
+    }
+
+    public User update(Long id, UserDTO userDTO) {
+        User entity = repository.findById(id).get();
+        updateData(entity, userDTO);
+        return repository.save(entity);
+    }
+
+    private void updateData(User entity, UserDTO dto){
+        entity      .setLogin(dto.getLogin());
+        entity.setPassword(dto.getPassword());
+    }
 }
